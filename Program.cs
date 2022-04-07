053c678ee84bf72f3b00ad037bdf243562e74d66
SHOCK <beponuka666@gmail.com>
2022-04-07 19:49:16 +0500

    Solution code written

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+
+string[] first_arr = { "1234", "123", "1567", "-2", "computer science", "new" };
+
+string[] second_arr = first_arr.Where(e => e.Length < 4).ToArray();
+
+Console.WriteLine(String.Join(' ', second_arr));
